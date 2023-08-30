315c3690b16343db722d1608af36d5772f8ee040
Nikita <niker217@mail.ru>
2023-08-30 14:35:40 +0700

    Help_functions.h

---
+++ Help_functions.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
+
+#include <assert.h>
+
+uint16_t svipe(uint8_t* buf, uint16_t len) {
+    uint16_t rev = NULL;
+    for (int i = 0; i < 2; i++) {
+        rev = (rev << 8) + buf[i];
+    };
+    return rev;
+}
